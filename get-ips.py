e04f98c4084234b5413d7087644598596057ee83
ChuckNorr1s <117844734+ChuckNorr1s@users.noreply.github.com>
2023-05-21 11:34:33 +0000

    fixed banner error get-ips.py

--- get-ips.py
+++ get-ips.py
@@ -11,5 +11,5 @@ if len(sys.argv) < 2:
 network_range = sys.argv[1]
 
 # Run masscan to scan for open ports on the target IP range
-command = f"sudo masscan -p80,443 --range {network_range} --rate=1000 --banners --max-rate 10000 --script=banner --wait 0 -oG - | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sed '/\b\(25[0-5]\|2[0-4][0-9]\|[01]\?[0-9][0-9]\?\)\(\.\(25[0-5]\|2[0-4][0-9]\|[01]\?[0-9][0-9]\?\)\)\{3\}\b/!d' > ips.txt"
+command = f"sudo masscan -p80,443 --range {network_range} --rate=1000 --banners --max-rate 10000 --wait 0 -oG - | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sed '/\b\(25[0-5]\|2[0-4][0-9]\|[01]\?[0-9][0-9]\?\)\(\.\(25[0-5]\|2[0-4][0-9]\|[01]\?[0-9][0-9]\?\)\)\{3\}\b/!d' > ips.txt"
 subprocess.run(command, shell=True)
